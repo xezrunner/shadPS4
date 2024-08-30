e712b8b824f09a386d01e678aeb9035b9ee56124
jnack <55568980+jnackmclain@users.noreply.github.com>
2024-08-30 12:50:59 -0500

    forgot the hashes

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -70,7 +70,7 @@ const ComputePipeline* PipelineCache::GetComputePipeline() {
 // 0xce54e4ddULL - Rock Band 4 V_MOVRELS_B32 - DO NOT USE, makes notes invisible
 
 bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
-    static constexpr std::array<u64, 2> skip_hashes = {0x3f84ec55ULL, 0xc7f34c4fULL};
+    static constexpr std::array<u64, 5> skip_hashes = {0x3f84ec55ULL, 0xc7f34c4fULL, 0x28080e22ULL, 0xc7f34c4fULL, 0x13a1d5fcULL};
     if (std::ranges::contains(skip_hashes, shader_hash)) {
         LOG_WARNING(Render_Vulkan, "Skipped {} shader hash {:#x}.", shader_type, shader_hash);
         return true;
